d73250e4e6560b59ccce28df728dd12850568dd4
WestonElliott <Westonini101@gmail.com>
2019-09-25 23:33:27 -0700

    - Created placeholder buttons and note sprites - Created NoteScroller script. (Nothing added except beatTempo calculation.

---
+++ 8-Beats/Assets/Scripts/NoteScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteScroller : MonoBehaviour
+{
+    public float bpm;        //BPM of the song that's to be entered in the inspector
+    private float beatTempo; //A.K.A. beats per second. Used for note scroll speed.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Divides the BPM by 60 seconds to get the beats per second.
+        beatTempo = bpm / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
