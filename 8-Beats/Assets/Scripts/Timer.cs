ff5fd8506a112c1d58016741276822f40a8e54df
WestonElliott <Westonini101@gmail.com>
2019-09-29 01:58:59 -0700

    - Started calculating the timings of the beat and the position of the song within the "Timer" script. - Created a "NoteScroller" script to get the notes moving. I'm currently using a Lerp function to move them since I read it's more accurate that way. - Created notes that spawn at four different points and scroll in one of four directions towards its respective button. - Imported a simple song I made as a test song.
    Things may look a bit messy under the hood right now but I do plan on cleaning it up soon. My main goal was just to test things out and see if they worked.

---
+++ 8-Beats/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float bpm;                  //BPM of the song that's to be entered in the inspector
+    public float songPos;              //Current song position in seconds  
+    public float songPosInBeats;  //Current song position in beats
+    public float secondsSinceStart;    //How many seconds have passed since the song started
+    public float spb;                  //Beats per second. Calculated by simply dividing the BPM by 60f.
+
+    public float firstBeatOffset;      //The offset to the first beat of the song in seconds.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Number of seconds between each beat.   
+        spb = 60f / bpm;
+
+        //Saves the time as a float when the song first starts.
+        secondsSinceStart = (float)AudioSettings.dspTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //determine how many seconds since the song started
+        songPos = (float)(AudioSettings.dspTime - secondsSinceStart - firstBeatOffset);
+
+        //determine how many beats since the song started. constantly set it equal to songPosition / bps so that it doesn't offset over time.
+        songPosInBeats = songPos / spb;
+    }
+}
