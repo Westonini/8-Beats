c73fd19ca6373971f2cb85b8d88d5e7588d83e4a
glimarie <gmariemapa@gmail.com>
2019-10-05 21:17:44 -0700

    - Created a new scene called "CloverDemo" - Created active player input buttons shaped as hearts - Note scroller is a go with clover / music notes

---
+++ 8-Beats/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private SpriteRenderer spriteRender;
+    public Sprite normLeaf;
+    public Sprite darkLeaf;
+
+    public KeyCode pressedKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(pressedKey))
+        {
+            spriteRender.sprite = darkLeaf;
+        }
+
+        if(Input.GetKeyUp(pressedKey))
+        {
+            spriteRender.sprite = normLeaf;
+        }
+    }
+}
