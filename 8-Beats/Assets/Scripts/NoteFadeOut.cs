397926a9d9d0654b0af21649cbf9560dfe2756f5
WestonElliott <Westonini101@gmail.com>
2019-10-07 00:34:50 -0700

    - Added NoteFadeOut script - The FadeAndDestroy IEnumerator within the script will do a fade out animation for the note and then destroy it. - Currently it's called when a note reaches the center of the clover (coordinates (0, 0)).
    - Created Animation folder with AnimationController and Animations subfolders
    
    - Still messing around with NoteScroller a bit. The script works currently, but things can get buggy if we move around the positions of the buttons and spawners so I'm trying to implement some sort of universal fix to that.

---
+++ 8-Beats/Assets/Scripts/NoteFadeOut.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Fades out and destroys the ntoe gameobject when the note reaches its final endpoint
+public class NoteFadeOut : MonoBehaviour
+{
+    Animator noteAnim;         //The note's animation component
+    NoteScroller noteScroller; //The note's NoteScroller script
+
+    //This method gets called before the Start() method and is good for getting references
+    void Awake()
+    {
+        //Get the note's components by simply getting the components that are on the same gameobject as this script
+        noteAnim = GetComponent<Animator>();
+        noteScroller = GetComponent<NoteScroller>();
+    }
+
+    //Subscribe/Unsubscribe to _noteReachedDestination to call FadeHandeler() whenever _noteReachedDestination gets called.
+    //Gets called when this gameobject is enabled
+    private void OnEnable()
+    {
+        noteScroller._noteReachedDestination += FadeHandeler;
+    }
+    //Gets called when this gameobject is disabled
+    private void OnDisable()
+    {
+        noteScroller._noteReachedDestination -= FadeHandeler;
+    }
+
+    //Starts the FadeAndDestroy coroutine
+    void FadeHandeler()
+    {
+        StartCoroutine("FadeAndDestroy");
+    }
+
+    //FADEOUT+DESTROY NOTE
+    IEnumerator FadeAndDestroy()
+    {
+        noteAnim.SetBool("FadeOut", true);      //Start the FadeOut animation of the note
+        yield return new WaitForSeconds(0.2f);  //Wait 0.2 seconds before doing the following code
+        Destroy(gameObject);                    //Destroy this note gameobject
+    }
+}
--- 8-Beats/Assets/Scripts/NoteScroller.cs
+++ 8-Beats/Assets/Scripts/NoteScroller.cs
@@ -5,44 +5,68 @@ using UnityEngine;
 //This class makes the note objects move towards the button objects.
 public class NoteScroller : MonoBehaviour
 {
-    public GameObject spawnPoint; //The gameobject at which the note will spawn and start its interpolation from.
-    public GameObject endPoint;   //the gameobject at which the note will stop its interpolation at.
+    public GameObject spawnPos; //The gameobject at which the note will spawn and start its interpolation from.
+    public GameObject buttonPos;   //the gameobject at which the note will stop its interpolation at.
     private float distanceGained; //The note's distance from start to finish
-    private bool reachedDestination; //Checks if the note has reached the button
+    private bool reachedButtonPos; //Checks if the note has reached the button
+    private bool reachedEndPos;       //Checks to see if the note has reached its endpoint
 
     public float beatOfNote;      //The beat of the note in the song
 
+    public delegate void NoteReachedDestination();
+    public event NoteReachedDestination _noteReachedDestination;            //Event to be invoked when the note reaches its final destination (0, 0)
+
     //Update is called once per frame
     void Update()
+    {
+        //Call all the following methods every frame
+        InterpolateNote();
+        InterpolationEndCheck();
+        DebugInfo();
+    }
+
+    //NOTE MOVER
+    void InterpolateNote()
     {
         //Updates the distanceGained every frame. This is what essentially sets the speed of the notes.
         //The speed of the notes is determined by the songPosInBeats and noteSlowness values from Timer. noteSlowness, like the name implies, will slow down the notes. A higher value = slower notes.
         distanceGained = ((Timer.songPosInBeats + (Timer.noteSlowness - 0.1f)) - beatOfNote) / Timer.noteSlowness - 0.1f;
 
-        //Interpolate the notes(move them) from the spawnPoint gameObject to the endPoint gameObject.
-        transform.position = Vector2.Lerp(spawnPoint.transform.position, new Vector2(0, 0), distanceGained);
-
-        //Check if the note's position is within 0.04f distance from its button's position. The note and button are never at an absolutly exact same position as each other, so this is as close as we can get.
-        //If the note speed is too fast it won't be able to detect it.
-        if (Vector2.Distance(transform.position, endPoint.transform.position) <= 0.04f)
-        {            
-            if (!reachedDestination) //Do a Debug.Log that shows some information once it approximately reaches the button position
-            {
-                Debug.Log("Beat of note: " + beatOfNote + "        Song position in beats: " + Timer.songPosInBeats);
-            }
-            reachedDestination = true;
-        }
-
-        //If the note reaches the center of the clover, destory it.
-        if (transform.position == new Vector3(0, 0, 0))
-        {
-            Destroy(gameObject);
-        }
-
+        //Interpolate the notes(move them) from the spawnPoint gameObject to the the coordinates (0, 0)
+        transform.position = Vector2.Lerp(spawnPos.transform.position, new Vector2(0, 0), distanceGained);
 
         /* -OLD CODE-
         distanceGained = ((Timer.songPosInBeats + Timer.noteSlowness) - beatOfNote) / Timer.noteSlowness;
         transform.position = Vector2.Lerp(spawnPoint.transform.position, endPoint.transform.position, distanceGained);
         */
+    }
+
+    //NOTE REACHED END-POSITION CHECK
+    void InterpolationEndCheck()
+    {
+        //Check for when the note's position is at the coordinates (0, 0)
+        if (transform.position == new Vector3(0, 0, 0) && !reachedEndPos)
+        {
+            //Call any methods that are currently subscribed to the _noteReachedDestination event
+            if (_noteReachedDestination != null)
+                _noteReachedDestination();
+
+            reachedEndPos = true;
+        }
+    }
+
+    //DEBUGGING INFORMATION
+    void DebugInfo()
+    {
+        //Check if the note's position is within 0.04f distance from its button's position. The note and button are never at an absolutly exact same position as each other, so this is as close as we can get.
+        //If the note speed is too fast it won't be able to detect it.
+        if (Vector2.Distance(transform.position, buttonPos.transform.position) <= 0.04f)
+        {
+            if (!reachedButtonPos) //Do a Debug.Log that shows some information once it approximately reaches the button position
+            {
+                Debug.Log("Beat of note: " + beatOfNote + "        Song position in beats: " + Timer.songPosInBeats);
+            }
+            reachedButtonPos = true;
+        }
     }
 }
