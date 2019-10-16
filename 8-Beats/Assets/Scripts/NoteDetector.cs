5830cb9029188e85d641a503800ef85fdaca2a72
glimarie <gmariemapa@gmail.com>
2019-10-15 18:47:51 -0700

    - Created new script called NoteDetector - NoteDetector is applied to the notes themselves - In other words, when the note hovers over the player input area, it gets destroyed if the player presses it on time within the area - Also fixed color coordination for notes & input area

---
+++ 8-Beats/Assets/Scripts/NoteDetector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteDetector : MonoBehaviour
+{
+    public bool canBePressed;
+  
+    public KeyCode pressedKey1;
+    public KeyCode pressedKey2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(pressedKey1) | Input.GetKeyDown(pressedKey2))
+        {
+            if(canBePressed)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+        }
+    }
+
+}
