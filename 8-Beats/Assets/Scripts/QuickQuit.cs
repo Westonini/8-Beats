1c07e323544b9761ed378705876cb3a6e85a1c11
Weston Elliott <Westonini101@gmail.com>
2019-10-16 17:35:07 -0700

    - Created QuickQuit script so you can press ESC during the song to quit the application

---
+++ 8-Beats/Assets/Scripts/QuickQuit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickQuit : MonoBehaviour
+{
+    void Update()
+    {
+        //If the player presses the "Escape" key the game will close.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
