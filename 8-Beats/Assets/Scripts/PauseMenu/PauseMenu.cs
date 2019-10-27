902158f7c69a00ee50a4fcd1d0bc4c8ac4bd1fa2
WestonElliott <Westonini101@gmail.com>
2019-10-27 00:56:23 -0700

    - Replaced QuickQuit script with PauseMenu script - PauseMenuButtons script created - PauseMenu script will open a pause menu and freeze the game when the player presses esc, and unpause it when they press esc again or press resume. - While the game is paused the player has the option to "Resume", "Retry", or go to the "Main Menu" - The PauseMenuButtons script is, as the name implies, code specifically for the button functions on the pause menu
    - BOTH ARE CURRENTLY DISABLED AS IT'S NOT YET FULLY COMPLETED

---
+++ 8-Beats/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool menuOpen = false;
+
+    public GameObject pauseMenu;
+    public AudioSource songSource;
+    private float currentAudioClipTime;
+
+    void Start()
+    {
+        menuOpen = false;
+    }
+
+    void Update()
+    {
+        //If the player presses the "Escape" key the game pause or unpause
+        if (Input.GetKeyDown(KeyCode.Escape) && !menuOpen)
+        {
+            EnablePauseMenu(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && menuOpen)
+        {
+            EnablePauseMenu(false);
+        }
+    }
+
+    //Enables or disables pause menu
+    public void EnablePauseMenu(bool state) //true = enable; false = disable
+    {
+        if (state) //Enable pause menu freeze time
+        {
+            pauseMenu.SetActive(true);                      //Enable pause menu panel gameobject
+            Time.timeScale = 0f; Time.fixedDeltaTime = 0f;  //Set the timescales to zero (freezes the game)
+            menuOpen = true;                                //Set the menuOpen bool to true
+
+            AudioListener.pause = true;                     //Pauses dspTime and mutes all audio
+        }
+        else      //Disable pause menu and unfreeze time
+        {
+            pauseMenu.SetActive(false);                     //Disable pause menu panel gameobject
+            Time.timeScale = 1f; Time.fixedDeltaTime = 1f;  //Set the timescales to one (unfreezes the game)
+            menuOpen = false;                               //Set the menuOpen bool to false
+
+            AudioListener.pause = false;                    //Unpauses dspTime and unmutes all audio
+        }
+    }
+}
---
+++ 8-Beats/Assets/Scripts/PauseMenu/PauseMenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuButtons : MonoBehaviour
+{
+    PauseMenu PM;
+
+    private void Awake()
+    {
+        PM = GetComponent<PauseMenu>(); //Get the PauseMenu script
+    }
+
+    //RESUME BUTTON
+    public void Resume()
+    {
+        PM.EnablePauseMenu(false); //Unpause the game
+    }
+
+    //RETRY BUTTON
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //Reload the current level
+    }
+
+    //MAIN MENU BUTTON
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0); //Load the main menu scene.
+    }
+}
